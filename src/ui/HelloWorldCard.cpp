4a1e97980aa98112b5e26e268cf8d79279928146
Danilo Campos <danilo@posthog.com>
2025-06-25 13:03:03 -0400

    Hello world card

---
+++ src/ui/HelloWorldCard.cpp
@@ -0,0 +1,38 @@
+#include "ui/HelloWorldCard.h"
+#include "Style.h"
+
+HelloWorldCard::HelloWorldCard(lv_obj_t* parent) : _card(nullptr), _label(nullptr) {
+    _card = lv_obj_create(parent); // Create the card container
+    lv_obj_set_size(_card, LV_PCT(100), LV_PCT(100)); // Make it fill its entire container
+    lv_obj_set_style_bg_color(_card, lv_color_black(), 0); // Set background to black
+    
+    _label = lv_label_create(_card); // Create a label on the card
+    lv_label_set_text(_label, "Hello, world!"); // Set the text content
+    lv_obj_set_style_text_color(_label, lv_color_white(), 0); // Set text color to white
+    lv_obj_set_style_text_font(_label, Style::largeValueFont(), 0); // Use the large font from Style
+    lv_obj_center(_label); // Center the label on the card
+}
+
+HelloWorldCard::~HelloWorldCard() {
+    if (_card) {
+        lv_obj_del_async(_card);
+        _card = nullptr;
+    }
+}
+
+bool HelloWorldCard::handleButtonPress(uint8_t button_index) {
+    // Toggle between black and white backgrounds
+    static bool isBlack = true;
+    
+    if (isBlack) {
+        lv_obj_set_style_bg_color(_card, lv_color_white(), 0);
+        lv_obj_set_style_text_color(_label, lv_color_black(), 0);
+    } else {
+        lv_obj_set_style_bg_color(_card, lv_color_black(), 0);
+        lv_obj_set_style_text_color(_label, lv_color_white(), 0);
+    }
+    
+    isBlack = !isBlack; // Toggle the state
+    
+    return true; // We handled the button press
+}
